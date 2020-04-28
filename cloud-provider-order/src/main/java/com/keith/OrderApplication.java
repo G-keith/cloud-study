4361211fcb4a2a6f48fd67b6aecac113eb02e583
keith <G-keith>
2020-04-28 14:57:39 +0800

    向eureka注册自己

--- cloud-provider-order/src/main/java/com/keith/OrderApplication.java
+++ cloud-provider-order/src/main/java/com/keith/OrderApplication.java
@@ -2,6 +2,7 @@ package com.keith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author keith
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020-04-28
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
