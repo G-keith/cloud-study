7213cdebf6f814e8afb6f4259e27029b733ba919
keith <G-keith>
2020-04-28 16:45:38 +0800

    增加服务发现Discovery接口

---
+++ cloud-provider-payment/src/main/java/com/keith/project/controller/DiscoveryController.java
@@ -0,0 +1,38 @@
+package com.keith.project.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-04-28
+ **/
+@Slf4j
+@RestController
+@RequestMapping(value = "/payment")
+public class DiscoveryController {
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping(value = "/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("***** element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+}
