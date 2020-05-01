e0d587bdb55de55fc0ad0ece2bfe73c814ee6cdb
keith <G-keith>
2020-05-01 13:52:32 +0800

    Hystrix熔断 使用@HystrixCommand进行异常服务降级

--- cloud-provider-payment-hystrix/src/main/java/com/keith/project/service/PaymentService.java
+++ cloud-provider-payment-hystrix/src/main/java/com/keith/project/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.keith.project.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -26,10 +28,23 @@ public class PaymentService {
      * @param id id
      * @return 线程信息
      */
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "4000")
+    })
     public String paymentInfoTimeOut(Integer id){
         int timeNumber = 3;
         try { TimeUnit.SECONDS.sleep(timeNumber); }catch (Exception e) {e.printStackTrace();}
         return "线程池："+Thread.currentThread().getName()+"   paymentInfo_TimeOut,id：  "+id+"\t"+"呜呜呜"+" 耗时(秒)"+timeNumber;
     }
+
+    /**
+     * 降级方法
+     * @param id id
+     * @return 降级方法信息
+     */
+    public String paymentInfoTimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"   系统繁忙, 请稍候再试  ,id：  "+id+"\t"+"哭了哇呜";
+    }
+
 
 }
