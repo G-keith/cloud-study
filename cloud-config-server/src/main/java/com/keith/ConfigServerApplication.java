616ca8ab9856a5370a0243c43a1ccb9be59444cc
keith <G-keith>
2020-05-02 15:19:47 +0800

    config  配置中心

---
+++ cloud-config-server/src/main/java/com/keith/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.keith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-05-02
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
