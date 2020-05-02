fa8c052fdeb01fc51e251601b13ce8ab674468dd
keith <G-keith>
2020-05-02 17:10:25 +0800

    优化客户端从服务端读取配置信息

--- cloud-config-client/src/main/java/com/keith/project/ConfigClientController.java
+++ cloud-config-client/src/main/java/com/keith/project/ConfigClientController.java
@@ -1,14 +1,17 @@
 package com.keith.project;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ *  RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
  * @author keith
  * @version 1.0
  * @date 2020-05-02
  **/
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
