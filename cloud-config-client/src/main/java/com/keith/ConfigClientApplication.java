962bb54f9bb20c77c21e821cc55f4e317019622c
keith <G-keith>
2020-05-02 16:47:08 +0800

    config  配置中心 客户端

---
+++ cloud-config-client/src/main/java/com/keith/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.keith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-05-02
+ **/
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ cloud-config-client/src/main/java/com/keith/project/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.keith.project;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-05-02
+ **/
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
