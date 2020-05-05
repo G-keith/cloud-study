96dc10cab24c84274dfb9983d0e7554a90744ea5
keith <G-keith>
2020-05-05 14:58:22 +0800

    config  配置中心 客户端案例2

---
+++ cloud-config-client2/src/main/java/com/keith/project/ConfigClientController.java
@@ -0,0 +1,28 @@
+package com.keith.project;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *  RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
+ * @author keith
+ * @version 1.0
+ * @date 2020-05-02
+ **/
+@RefreshScope
+@RestController
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "serverPort:"+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+}
