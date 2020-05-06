100121755d5ac58ea7639a712ffe61b700c96cc0
keith <G-keith>
2020-05-06 11:37:43 +0800

    cloud stream 引入分组，解决重复消费和数据持久化的问题

---
+++ cloud-stream-rabbitmq-consumer2/src/main/java/com/keith/project/controller/ReceiveMessageController.java
@@ -0,0 +1,27 @@
+package com.keith.project.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-05-06
+ **/
+@RestController
+@EnableBinding(Sink.class)
+public class ReceiveMessageController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        System.out.println(serverPort+":"+message.getPayload());
+
+    }
+}
