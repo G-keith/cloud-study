e59056d59e7876b93a880e03d46babdea3247cdd
keith <G-keith>
2020-05-08 09:54:07 +0800

    cloud  alibaba  服务调用

---
+++ alibaba-provider-order9000/src/main/java/com/keith/project/controller/OrderNacosController.java
@@ -0,0 +1,36 @@
+package com.keith.project.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2020-05-08
+ **/
+@Slf4j
+@RestController
+@RequestMapping(value = "/order")
+public class OrderNacosController
+{
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverUrl;
+
+    @GetMapping(value = "/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id)
+    {
+        return restTemplate.getForObject(serverUrl +"/payment/nacos/"+id,String.class);
+    }
+
+}
+
